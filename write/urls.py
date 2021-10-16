1b1f764d98857bfc50241fc03156a257f4aba293
Ahmad Khaefi <ahmadkhaefi@yahoo.com>
2021-10-16 15:52:03 +0330

    set initial templates

--- write/urls.py
+++ write/urls.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
 from django.urls import path
-from .views import *
+from . import views
+
+urlpatterns = [
+    path('', views.notes, name='notes')
+]
--- write/views.py
+++ write/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def notes(request):
+    context = {
+
+    }
+    return render(request, 'notes.html', context)
