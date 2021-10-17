cff1710dd65c904354dd1f31695dbc472cd4aeba
Ahmad Khaefi <ahmadkhaefi@yahoo.com>
2021-10-17 20:22:54 +0330

    add Shortnote model

--- write/models.py
+++ write/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Shortnote(models.Model):
+    STATUS = (
+        ('important', 'important'),
+        ('warning', 'warning'),
+        ('note', 'note')
+    )
+    text     = models.CharField(max_length=1000, blank=False, null=False)
+    status   = models.CharField(max_length=1000, blank=False, null=False, choices=STATUS, default='')
+    creation = models.DateTimeField(auto_now_add=True, blank=False, null=False)
+
+    def color(self):
+        if self.status == 'important':
+            return 'danger'
+        elif self.status == 'warning':
+            return 'warning'
+        else:
+            return 'primary'
+    def content(self):
+        if self.status == 'important':
+            return '<i class="fad fa-exclamation-circle"></i> important'
+        elif self.status == 'warning':
+            return '<i class="fad fa-exclamation-circle"></i> warning'
+        else:
+            return ''
+    def __str__(self):
+        return self.text
--- write/views.py
+++ write/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import *
 
 def notes(request):
+    shortnotes = Shortnote.objects.all()
+    if request.method == 'POST':
+        text = request.POST.get('write-input')
+        Shortnote.objects.create(
+            text = text,
+            status = 'important'
+        )
     context = {
-
+        'shortnotes': shortnotes
     }
     return render(request, 'notes.html', context)
